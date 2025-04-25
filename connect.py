b2ccc171af5a800e5c64ab5592f02262d93eb34d
Zack OConnor <zackary25@gmail.com>
2025-04-25 12:06:15 -0400

    Initial commit

---
+++ connect.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+connection = psycopg2.connect(database="test_db")
+cursor = connection.cursor()
+
+# Companies
+
+# Employees
+
+# Create
+
+# Read
+
+# Update
+
+# Delete
+
+connection.close()
